c0b62d5285ca3e906be4a53db5a47db07dded0a8
Manish-1415 <manishshinde1415@gmail.com>
2025-07-29 09:53:50 +0530

    Created connectDB for DB connection

--- src/db_connection/db.connect.ts
+++ src/db_connection/db.connect.ts
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
+import ApiError from "../utilitys/ApiError.utility.js";
 
-const connectDB = async () : Promise<string> => {
-    const dbConnect = await mongoose.connect(`${process.env.DB_CONNECTION_URL}/ecommerce_data`);
+const connectDB = async (): Promise<void> => {      //here i have returning string before , but this function return nothing , so void
+  try {
+    const dbConnect = await mongoose.connect(
+      `${process.env.DB_CONNECTION_URL}/ecommerce_data`
+    );
 
-    if()
-}
+    if (!dbConnect)
+      throw new ApiError(500, "Server Side Error occured during DB connection");
 
-
+    console.log("DB connected Successfully");
+    console.log(dbConnect.connection.host);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
